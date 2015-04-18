7cfafaa4e90d3f561c9eb3d502af7931b36abf71
Callum <callum.waddell@btinternet.com>
2015-04-18 12:33:16 +0100

    Basic Grav effect

--- LudumDare32/Assets/Scripts/Player.cs
+++ LudumDare32/Assets/Scripts/Player.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 
-    public Vector2 grav;
 
     Rigidbody2D rigidbody2D;
     BoxCollider2D boxCollider2D;
@@ -12,8 +11,15 @@ public class Player : MonoBehaviour
     public float moveForce = 5;
     public float jumpForce = 10;
 
-    public LayerMask ground;
- 
+    public LayerMask groundMask;
+
+    public GameObject crosshair;
+
+    public Vector2 grav;
+
+    public float maxHorzGrav = 9.81f;
+    public float maxVertGrav = 9.81f;
+
     // Use this for initialization
     void Start()
     {
@@ -24,13 +30,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        grav = Physics2D.gravity;
+        Movement();
 
-        RaycastHit2D grounded = Physics2D.Raycast(transform.position, -transform.up, boxCollider2D.size.y, ground);
+        GravEffect();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Boundary"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+    
+    void Movement()
+    {
+        RaycastHit2D grounded = Physics2D.Raycast(transform.position, -transform.up, boxCollider2D.size.y, groundMask);
 
         if (grounded.collider != null)
         {
-            Debug.Log(grounded.collider.name );
 
             if (Input.GetAxis("Horizontal") > 0)
             {
@@ -42,18 +60,34 @@ public class Player : MonoBehaviour
             }
 
 
-            if (Input.GetButtonDown("Jump") )
+            if (Input.GetButtonDown("Jump"))
             {
                 rigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
             }
         }
+
     }
 
-    void OnCollisionEnter2D(Collision2D coll)
+    void GravEffect()
     {
-        if(coll.collider.CompareTag("Boundary"))
-        {
-            Application.LoadLevel(Application.loadedLevel);
-        }
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = - Camera.main.transform.position.z;
+        crosshair.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+
+        float t = ( (mousePos.x - (float)Screen.width / 2)) / ((float)Screen.width / 2);
+        t = Mathf.Clamp(t, -1, 1);
+
+        grav.x = t * maxHorzGrav;
+
+        t = (mousePos.y - (float)Screen.height / 2) / ((float)Screen.height / 2);
+        t = Mathf.Clamp(t, -1, 1);
+
+        grav.y = t * maxVertGrav;
+
+
+        Physics2D.gravity = grav;
     }
+
+
 }
