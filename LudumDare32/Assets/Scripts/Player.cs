8bbcf017929c2dc5c61c9952755d73b2e3539b92
Callum <callum.waddell@btinternet.com>
2015-04-18 10:59:39 +0100

    Basic 2d movement set up

---
+++ LudumDare32/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Vector2 grav;
+
+    Rigidbody2D rigidbody2D;
+
+    public float moveForce = 5;
+    public float jumpForce = 10;
+
+     public GameObject groundCheck;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        grav = Physics2D.gravity;
+        
+        if( Input.GetAxis("Horizontal") > 0  )
+        {
+            rigidbody2D.AddForce(transform.right * moveForce);
+        } 
+        else  if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigidbody2D.AddForce(-transform.right * moveForce);
+        }
+
+        bool grounded = Physics2D.Linecast(transform.position, groundCheck.transform.position , LayerMask.NameToLayer("Ground") );
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            
+            rigidbody2D.AddForce(transform.up * jumpForce , ForceMode2D.Impulse);
+        }
+    }
+}
