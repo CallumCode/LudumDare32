8a28346875dda4965887f840dd693957ed2841e3
Callum <callum.waddell@btinternet.com>
2015-04-18 11:53:57 +0100

    Checks for ground and boundary

--- LudumDare32/Assets/Scripts/Player.cs
+++ LudumDare32/Assets/Scripts/Player.cs
@@ -1,44 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public Vector2 grav;
 
     Rigidbody2D rigidbody2D;
+    BoxCollider2D boxCollider2D;
 
     public float moveForce = 5;
     public float jumpForce = 10;
 
-     public GameObject groundCheck;
-
-	// Use this for initialization
-	void Start () 
+    public LayerMask ground;
+ 
+    // Use this for initialization
+    void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    // Update is called once per frame
+    void Update()
     {
         grav = Physics2D.gravity;
-        
-        if( Input.GetAxis("Horizontal") > 0  )
+
+        RaycastHit2D grounded = Physics2D.Raycast(transform.position, -transform.up, boxCollider2D.size.y, ground);
+
+        if (grounded.collider != null)
         {
-            rigidbody2D.AddForce(transform.right * moveForce);
-        } 
-        else  if (Input.GetAxis("Horizontal") < 0)
-        {
-            rigidbody2D.AddForce(-transform.right * moveForce);
+            Debug.Log(grounded.collider.name );
+
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                rigidbody2D.AddForce(transform.right * moveForce);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                rigidbody2D.AddForce(-transform.right * moveForce);
+            }
+
+
+            if (Input.GetButtonDown("Jump") )
+            {
+                rigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
+    }
 
-        bool grounded = Physics2D.Linecast(transform.position, groundCheck.transform.position , LayerMask.NameToLayer("Ground") );
-
-        if (Input.GetButtonDown("Jump") && grounded)
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.collider.CompareTag("Boundary"))
         {
-            
-            rigidbody2D.AddForce(transform.up * jumpForce , ForceMode2D.Impulse);
+            Application.LoadLevel(Application.loadedLevel);
         }
     }
 }
