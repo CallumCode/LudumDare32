73bc48dce30961e73b38aa721fa0d2909f8de32a
Callum <callum.waddell@btinternet.com>
2015-04-18 15:38:06 +0100

    add basic art
    animations  walk
    
    background
    crate

--- LudumDare32/Assets/Scripts/Player.cs
+++ LudumDare32/Assets/Scripts/Player.cs
@@ -15,16 +15,26 @@ public class Player : MonoBehaviour
 
     public GameObject crosshair;
 
+    public Animator animator;
+
     public Vector2 grav;
 
     public float maxHorzGrav = 9.81f;
     public float maxVertGrav = 9.81f;
 
+    public GameObject sprite;
+    bool facingRight = true;
+
+    public float airDrag = 0.5f;
+     public float groundDrag = 1;
+
+     public GameObject groundCheck; 
     // Use this for initialization
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
+        animator = GetComponentInChildren< Animator > ();
     }
 
     // Update is called once per frame
@@ -32,7 +42,7 @@ public class Player : MonoBehaviour
     {
         Movement();
 
-        GravEffect();
+      //  GravEffect();
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -46,9 +56,32 @@ public class Player : MonoBehaviour
     void Movement()
     {
         RaycastHit2D grounded = Physics2D.Raycast(transform.position, -transform.up, boxCollider2D.size.y, groundMask);
+        grounded = Physics2D.CircleCast(groundCheck.transform.position, boxCollider2D.size.x / 1.8f, -transform.up, boxCollider2D.size.x / 1.8f , groundMask);
+
+        animator.SetFloat("Speed", rigidbody2D.velocity.magnitude);
+
+         if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("PlayerWalk"))
+         {
+            Vector3 theScale = transform.localScale;
+
+            if ( rigidbody2D.velocity.x > 0 && !facingRight)
+            {
+                Flip();                 
+            }
+            else if (rigidbody2D.velocity.x < 0 && facingRight)
+            {
+                Flip();
+            } 
+         }
+            
 
         if (grounded.collider != null)
         {
+            Debug.Log(grounded.collider.name);
+
+            rigidbody2D.drag = groundDrag;
+
+            animator.SetBool("TouchGround", true);
 
             if (Input.GetAxis("Horizontal") > 0)
             {
@@ -65,6 +98,12 @@ public class Player : MonoBehaviour
                 rigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
             }
         }
+        else
+        {
+            rigidbody2D.drag = airDrag;
+
+            animator.SetBool("TouchGround" ,false);
+        }
 
     }
 
@@ -75,7 +114,7 @@ public class Player : MonoBehaviour
         mousePos.z = - Camera.main.transform.position.z;
         crosshair.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
 
-        float t = ( (mousePos.x - (float)Screen.width / 2)) / ((float)Screen.width / 2);
+        float t =    ( (mousePos.x - (float)Screen.width / 2)) / ((float)Screen.width / 2);
         t = Mathf.Clamp(t, -1, 1);
 
         grav.x = t * maxHorzGrav;
@@ -86,8 +125,17 @@ public class Player : MonoBehaviour
         grav.y = t * maxVertGrav;
 
 
-        Physics2D.gravity = grav;
+        Physics2D.gravity = grav;   
     }
 
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
 
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
